594820b4a07a2ce0b22e6ae4b7d0e98c8d1fbe7b
harrispirie <hpirie@live.com>
2016-07-14 15:55:40 -0400

    Initial commit
    Includes the capability to read Nannies files and some basic tools for
    data analysis in python.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name = 'stmpy',
+    author = 'Hoffman Lab',
+    version = '0.1',
+    description = 'A collection of data anaysis tools for STM',
+    licence = 'Harvard University',
+    packages = ['stmpy']
+    )
