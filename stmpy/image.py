91f75a3814ced8bedb93312ebd15381ab2f27e8b
harrispirie <hpirie@live.com>
2016-09-06 13:44:39 -0400

    Added speed and zoom control for movies.

--- stmpy/image.py
+++ stmpy/image.py
@@ -4,7 +4,20 @@ import pylab as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib import cm
 
-def write_animation(F, fileName, saturation=2, label=None, cmap=None):
+def write_animation(F, fileName, saturation=2, label=None, cmap=None, speed=8,
+                    zoom=1, codec='prores'):
+    '''
+    Create a movie from a 3D data set and save it in the working directory.  Intended    for visualising DOS maps.
+
+    Iterates through the first index in the data set to create an animation.
+    Make sure you include '.mov' in the file name.  Default codec is prores
+    (apple), other codecs can be specified. Make sure you have ffmpeg installed
+    (e.g. through Homebrew) before running.
+
+    Usage: write_animation(data.LIY, 'MOV_DOSmap.mov', saturation=2,
+    label=data.en, cmap=cm.bone_r, speed=8, zoom=1, codec='prores')
+    '''
+
     boxProperties1 = dict(boxstyle='square', facecolor='w', 
                             alpha=0.8, linewidth=0.0)
     textOptions1 = dict(fontsize=14, color = 'k', 
@@ -17,8 +30,8 @@ def write_animation(F, fileName, saturation=2, label=None, cmap=None):
     ax = plt.subplot(111)
     ax.set_xticks([])
     ax.set_yticks([])
-    plt.xlim(-1,1)
-    plt.ylim(-1,1)
+    plt.xlim(-1.0/zoom, 1.0/zoom)
+    plt.ylim(-1.0/zoom, 1.0/zoom)
     im = plt.pcolormesh(x, x, F[0], cmap=cmap)
     sp.saturate(saturation)
     if label is not None:
@@ -37,7 +50,7 @@ def write_animation(F, fileName, saturation=2, label=None, cmap=None):
         return [im]
     fig.tight_layout()
     ani = FuncAnimation(fig, animate, init_func=init, frames = F.shape[0])
-    ani.save(fileName, codec='prores', dpi=200, fps=6)
+    ani.save(fileName, codec=codec, dpi=200, fps=speed)
 
 
 
