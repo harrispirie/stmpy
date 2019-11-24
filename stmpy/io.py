b9bbc58bad55777ef9ce26cdd00e8de8fb790eba
harrispirie <hpirie@live.com>
2019-11-24 13:45:03 -0500

    Added compatibility to nice_units()

--- stmpy/io.py
+++ stmpy/io.py
@@ -6,13 +6,13 @@ Read and write common file types.
 
 Contents:
     load()  -   Load supported data into python
-    save()  -   Save python data to disk. 
+    save()  -   Save python data to disk.
 
 Version history:
-    1.0     2018-03-02  - HP : Initial release. 
+    1.0     2018-03-02  - HP : Initial release.
     1.1     2018-10-10  - HP : Python 3 compatibility
 
-TO DO: 
+TO DO:
     - Add support for .mat files
     - Rewrite load_3ds() to buffer data and improve efficiency.
 '''
@@ -39,15 +39,15 @@ def load(filePath, biasOffset=True, niceUnits=False):
     '''
     Loads data into python. Please include the file extension in the path.
 
-    Supported extensions: 
-        .spy    -   STMPY generic data format. 
+    Supported extensions:
+        .spy    -   STMPY generic data format.
         .3ds    -   NANONIS grid data. Commonly used for DOS maps.
-        .sxm    -   NANONIS scan data. Commonly used for topographys. 
-        .dat    -   NANONIS data file. Commonly used for bias spectroscopy. 
-        .nsp    -   NANONIS long term spectum data type. 
-        .nvi    -   NISTview image data, used for topography data.  
-        .nvl    -   NISTview layer data, used for 3D DOS maps. 
-        .asc    -   ASCII file type. 
+        .sxm    -   NANONIS scan data. Commonly used for topographys.
+        .dat    -   NANONIS data file. Commonly used for bias spectroscopy.
+        .nsp    -   NANONIS long term spectum data type.
+        .nvi    -   NISTview image data, used for topography data.
+        .nvl    -   NISTview layer data, used for 3D DOS maps.
+        .asc    -   ASCII file type.
 
 
     For .3ds and .dat file types there is an optional flag to correct for bias offset
@@ -65,21 +65,21 @@ def load(filePath, biasOffset=True, niceUnits=False):
         spyObject  - Custom object with attributes appropriate to the type of
                       data and containing experiment parameters in a header.
 
-    History: 
-        2016-07-14  - HP : Initial commit. 
+    History:
+        2016-07-14  - HP : Initial commit.
         2016-07-15  - HP : Added support for NVL and NVI files.
         2016-07-29  - HP : Added support for rectangular DOS maps.
         2016-08-02  - HP : Added support for single line DOS maps.
-        2016-08-09  - HP : Added bias offset for DAT files. 
-        2016-09-14  - HP : Added compatibility for incomplete data sets. 
-        2016-11-01  - HP : Added support for specific ASCII files. 
+        2016-08-09  - HP : Added bias offset for DAT files.
+        2016-09-14  - HP : Added compatibility for incomplete data sets.
+        2016-11-01  - HP : Added support for specific ASCII files.
         2017-01-13  - HP : Improved loading of DAT files.
-        2017-03-27  - RL : Added support for NSP files. 
+        2017-03-27  - RL : Added support for NSP files.
         2017-06-08  - HP : Use sensible units when loading data.
         2017-06-16  - JG : Improve handling of multi-sweep DAT files.
-        2017-08-11  - HP : Added support for non-linear bias sweep. 
-        2017-08-17  - JG : Added support for general ASCII files. 
-        2017-08-24  - HP : Better searching for Z attribute in DOS maps. 
+        2017-08-11  - HP : Added support for non-linear bias sweep.
+        2017-08-17  - JG : Added support for general ASCII files.
+        2017-08-24  - HP : Better searching for Z attribute in DOS maps.
         2017-10-03  - HP : Improved reading of DAT files
         2018-03-02  - HP : VERSION  1.0 - Unified to a single SPY class.
         2018-10-10  - HP : Python 3 compatibility
@@ -104,10 +104,10 @@ def load(filePath, biasOffset=True, niceUnits=False):
         if niceUnits:
             dataObject = _nice_units(dataObject, extension)
         return dataObject
-    
+
     elif extension in ['spy', 'sxm', 'nvi', 'nvl', 'nsp', 'asc']:
         return eval(loadFn)(filePath)
-    
+
   #  elif filePath.endswith('.mat'):
   #      raw_mat = matio.loadmat(filePath)
   #      mappy_dict = {}
@@ -122,7 +122,7 @@ def load(filePath, biasOffset=True, niceUnits=False):
   #      if len(mappy_dict) == 1: return mappy_dict[mappy_dict.keys()[0]]
   #      else: return mappy_dict
 
-    else: 
+    else:
         raise IOError('ERR - File type {:} not supported.'.format(extension))
 
 
@@ -130,20 +130,20 @@ def save(data, filePath, objects=[]):
     '''
     Save python data to file. Please include the file extension in the path.
 
-    Currently supports: 
-        .spy    -   STMPY generic data format. 
+    Currently supports:
+        .spy    -   STMPY generic data format.
 
-    Inputs: 
+    Inputs:
         data        - Required : Any python data/object/list/...
         filePath    - Required : str. Path where the file will be saved.
-        objects     - Optional : lst. Only objects with a __class__ in this 
-                                 list (and Spy objects) can be saved.  
+        objects     - Optional : lst. Only objects with a __class__ in this
+                                 list (and Spy objects) can be saved.
 
-    Returns: 
+    Returns:
         None
 
-    History: 
-        2018-03-02  - HP : Initial commit. 
+    History:
+        2018-03-02  - HP : Initial commit.
         2018-03-08  - HP : Added support for multi-line strings.
         2018-10-10  - HP : Python 3 compatibility
     '''
@@ -153,8 +153,8 @@ def save(data, filePath, objects=[]):
         raise IOError('Please include file extension in path.')
     saveFn = 'save_' + extension
     if extension in ['spy']:
-        eval(saveFn)(data, filePath, objects)        
-    else: 
+        eval(saveFn)(data, filePath, objects)
+    else:
         raise IOError('ERR - File type {:} not supported.'.format(extension))
 
 
@@ -188,7 +188,7 @@ def _nice_units(data, fileType):
 
     History:
         2017-08-10  - HP : Comment: Missing a factor of 2, phase error not
-                           justified 
+                           justified
         2018-11-13  - HP : Add nice_units to .dat files
 
     '''
@@ -197,19 +197,22 @@ def _nice_units(data, fileType):
             gFit = X * data.didv / lockInMod
             err = np.absolute(gFit - didv)
             return np.log(np.sum(err**2))
-        lockInMod = float(data.header['Lock-in>Amplitude'])
+        try:
+            lockInMod = float(data.header['Lock-in>Amplitude'])
+        except KeyError:
+            lockInMod = 1 # Doesn't matter due to minimization.
        # current = np.mean(data.I, axis=(1,2))
         didv = np.gradient(data.iv) / np.gradient(data.en)
         result = minimize(chi, 1)
-        data.to_nS = result.x / lockInMod * 1e9 
+        data.to_nS = result.x / lockInMod * 1e9
         data.didv *= data.to_nS
-        try: 
+        try:
             data.LIY *= data.to_nS
             data.didvStd *= data.to_nS
         except AttributeError:
             pass # Just means that this is a .dat rather than a .3ds
         phi = np.arccos(1.0/result.x)
-    
+
     def use_nm(data):
         fov = [float(val) for val in data.header['Scan>Scanfield'].split(';')]
         data.x = 1e9 * np.linspace(0, fov[2], data.Z.shape[1])
@@ -220,7 +223,7 @@ def _nice_units(data, fileType):
         data._pxToNm = data.x[-1]/len(data.x)
         data._pxToInvNm = data.qx[-1]/len(data.qx)
         print('WARNING: I am not 100% sure that the q scale is right...')
-    
+
     if fileType in ['3ds', 'dat']:
         use_nS(data)
     if fileType in ['3ds', 'sxm']:
@@ -238,7 +241,7 @@ def _make_attr(self, attr, names, data):
         names   - Required : List of names to search for
         data    - Required : Name of a current attribute in which the new
                              attribute is stored.
-    
+
     Returns:
         1   - If successfully added the attribute
         0   - If name is not found.
@@ -264,7 +267,7 @@ def save_spy(data, filePath, objects=[]):
     def stew(fileObj, val):
         'Quickly write binary strings with utf-8 encoding.'
         return fileObj.write(bytearray(val.encode('utf-8')))
-    
+
     def write_npy(name, npy):
         if npy.dtype.name == 'object':
             stew(fileObj, 'OAR=' + name + '\n' + str(npy.shape) + '\n')
@@ -282,7 +285,7 @@ def save_spy(data, filePath, objects=[]):
 
     def write_dic(name, dic):
         stew(fileObj, 'DIC=' + name + '\n')
-        for name, item in dic.items():        
+        for name, item in dic.items():
             write_item(name, item)
         stew(fileObj, ':DIC_END:\n')
 
@@ -296,7 +299,7 @@ def save_spy(data, filePath, objects=[]):
         stew(fileObj, 'STR=' + name + '\n' + val + '\n:STR_END:\n')
         #stew(bytearray(val.encode('utf-8')) + '\n')
         #stew(':STR_END:\n')
-    
+
     def write_byt(name, byt):
         stew(fileObj, 'BYT=' + name + '\n')
         fileObj.write(byt)
@@ -320,7 +323,7 @@ def save_spy(data, filePath, objects=[]):
         stew(fileObj, 'BOL=' + name + '\n')
         fileObj.write('NOTWORKING')
 
-    def write_item(name, item):        
+    def write_item(name, item):
         if type(item).__module__ == np.__name__:
             write_npy(name, item)
         elif isinstance(item, dict):
@@ -329,7 +332,7 @@ def save_spy(data, filePath, objects=[]):
             write_lst(name, item)
         elif isinstance(item, tuple):
             print('Tuples present...')
-        elif hasattr(item, 'read'): 
+        elif hasattr(item, 'read'):
             pass
         elif isinstance(item, str):
             write_str(name, item)
@@ -351,7 +354,7 @@ def save_spy(data, filePath, objects=[]):
                 write_num(name, item)
         else:
             raise(TypeError('Item {:} {:} not supported.'.format(name, type(item))))
-    
+
     fileObj = open(filePath, 'wb')
     stew(fileObj, 'SPY: Stmpy I/O, Version=' + str(__version__) + '\n')
     objects.append(Spy)
@@ -363,13 +366,13 @@ def save_spy(data, filePath, objects=[]):
 ####    ____LOAD FUNCTIONS____    ####
 
 def load_spy(filePath):
-    ''' Load .spy files into python''' 
+    ''' Load .spy files into python'''
     def read_npy(fileObj):
         npy = np.load(fileObj)
         if npy.shape == ():
             npy = npy.flatten()[0]
         return npy
-    
+
     def read_oar(fileObj):
         line = fileObj.readline().strip().decode('utf-8')
         shape = eval(line)
@@ -378,7 +381,7 @@ def load_spy(filePath):
             line = fileObj.readline().strip().decode('utf-8')
             oar[ix] = read_obj(fileObj)
         return oar.reshape(shape)
-    
+
     def read_obj(fileObj):
         obj = Spy()
         while True:
@@ -389,7 +392,7 @@ def load_spy(filePath):
             key, val = line.split('=')
             setattr(obj, val, read_item(fileObj, key))
         return obj
-    
+
     def read_dic(fileObj):
         dic = {}
         while True:
@@ -399,7 +402,7 @@ def load_spy(filePath):
             key, val = line.split('=')
             dic[val] = read_item(fileObj, key)
         return dic
-    
+
     def read_lst(fileObj):
         lst = []
         while True:
@@ -409,7 +412,7 @@ def load_spy(filePath):
             key, val = line.split('=')
             lst.append(read_item(fileObj, key))
         return lst
-    
+
     def read_str(fileObj):
         st = ''
         while True:
@@ -429,12 +432,12 @@ def load_spy(filePath):
         fmt = fileObj.read(2)
         num = unpack(fmt, fileObj.read(calcsize(fmt)))[0]
         return num
-    
+
     def read_cpx(fileObj):
         real = unpack('>f', fileObj.read(4))[0]
         imag = unpack('>f', fileObj.read(4))[0]
         return complex(real, imag)
-    
+
     def read_item(fileObj, key):
         if   key == 'NPY':
             item = read_npy(fileObj)
@@ -442,7 +445,7 @@ def load_spy(filePath):
             item = read_oar(fileObj)
         elif key == 'OBJ':
             item = read_obj(fileObj)
-        elif key == 'DIC': 
+        elif key == 'DIC':
             item = read_dic(fileObj)
         elif key == 'LST':
             item = read_lst(fileObj)
@@ -476,15 +479,15 @@ def load_spy(filePath):
 
 def load_3ds(filePath):
     '''Load Nanonis 3ds into python.'''
-    try: 
+    try:
         fileObj = open(filePath, 'rb')
-    except:  
+    except:
         raise NameError('File not found.')
     self = Spy()
     self.header={}
     while True:
         line = fileObj.readline().strip().decode('utf-8')
-        if line == ':HEADER_END:': 
+        if line == ':HEADER_END:':
             break
         splitLine = line.split('=')
         self.header[splitLine[0]] = splitLine[1]
@@ -521,20 +524,20 @@ def load_3ds(filePath):
     dataRead = fileObj.tell()
     fileObj.read()
     allData = fileObj.tell()
-    if dataRead == allData: 
+    if dataRead == allData:
         print('File import successful.')
-    else: 
+    else:
         print('ERR: Did not reach end of file.')
     fileObj.close()
-    
+
     LIYNames =  ['LIY 1 omega (A)', 'LIY 1 omega [AVG] (A)', 'LI Demod 1 Y (A)', 'LI Demod 2 Y (A)','LI Demod 3 Y (A)']
     if _make_attr(self, 'LIY', LIYNames, 'grid'):
         self.didv = np.mean(self.LIY, axis=(1,2))
         self.didvStd = np.std(self.LIY, axis=(1,2))
     else:
-        print('ERR: LIY AVG channel not found, resort to manual ' + 
+        print('ERR: LIY AVG channel not found, resort to manual ' +
               'definitions.  Found channels:\n {:}'.format(self.grid.keys()))
-    
+
     _make_attr(self, 'I',  ['Current (A)', 'Current [AVG] (A)'], 'grid')
     if _make_attr(self, 'Z',  ['Z (m)', 'Z [AVG] (m)'], 'grid'):
         self.Z = self.Z[0]
@@ -542,7 +545,7 @@ def load_3ds(filePath):
         _make_attr(self, 'Z', ['Scan:Z (m)'], 'scan')
         print('WARNING: Using scan channel for Z attribute.')
     self.iv = np.mean(self.I, axis=(1,2))
-    try:     
+    try:
         self.en = np.mean(self.grid['Bias [AVG] (V)'], axis=(1,2))
     except KeyError:
         print('WARNING: Assuming energy layers are evenly spaced.')
@@ -555,9 +558,9 @@ def load_3ds(filePath):
 
 def load_sxm(filePath):
     ''' Load Nanonis SXM files into python. '''
-    try: 
+    try:
         fileObj = open(filePath, 'rb')
-    except:  
+    except:
         raise NameError('File not found.')
     self = Spy()
     self.header={}
@@ -626,9 +629,9 @@ def load_sxm(filePath):
 
 def load_dat(filePath):
     ''' Load Nanonis SXM files into python. '''
-    try: 
+    try:
         fileObj = open(filePath, 'rb')
-    except:  
+    except:
         raise NameError('File not found.')
     self = Spy()
     self.header={}
@@ -636,9 +639,9 @@ def load_dat(filePath):
     while True:
         line = fileObj.readline().decode('utf-8')
         splitLine = line.split('\t')
-        if line[0:6] == '[DATA]': 
+        if line[0:6] == '[DATA]':
             break
-        elif line.rstrip() != '': 
+        elif line.rstrip() != '':
             self.header[splitLine[0]] = splitLine[1]
     channels = fileObj.readline().decode('utf-8').rstrip().split('\t')
     allData = []
@@ -651,12 +654,12 @@ def load_dat(filePath):
     dataRead = fileObj.tell()
     fileObj.read()
     finalRead = fileObj.tell()
-    if dataRead == finalRead: 
+    if dataRead == finalRead:
         print('File import successful.')
-    else: 
+    else:
         print('ERR: Did not reach end of file.')
     fileObj.close()
-    _make_attr(self, 'didv', 
+    _make_attr(self, 'didv',
             ['LIY 1 omega (A)', 'LIY 1 omega [AVG] (A)'], 'channels')
     _make_attr(self, 'iv', ['Current (A)', 'Current [AVG] (A)'],
     'channels')
@@ -684,15 +687,15 @@ def load_dat(filePath):
 
 def load_nsp(filePath):
     '''UNTESTED - Load Nanonis Long Term Specturm into python.'''
-    try: 
+    try:
         fileObj = open(filePath, 'rb')
-    except:  
+    except:
         raise NameError('File not found.')
     self = Spy()
     self.header={}
     while True:
         line = fileObj.readline().strip().decode('utf-8')
-        if line == ':HEADER_END:': 
+        if line == ':HEADER_END:':
             break
         elif re.match('^:.*:$', line):
             tagname = line[1:-1]
@@ -702,15 +705,15 @@ def load_nsp(filePath):
             except:
                 self.header[tagname] = line.split('\t')[0]
 
-    self.freq = np.linspace(0, 
+    self.freq = np.linspace(0,
             np.round(float(self.header['DATASIZECOLS'])*float(self.header['DELTA_f'])),
             float(self.header['DATASIZECOLS']))
-    
-    self.start = datetime.strptime(self.header['START_DATE'] + 
+
+    self.start = datetime.strptime(self.header['START_DATE'] +
             self.header['START_TIME'],'%d.%m.%Y%H:%M:%S')
-    self.end = datetime.strptime(self.header['END_DATE'] + 
+    self.end = datetime.strptime(self.header['END_DATE'] +
             self.header['END_TIME'],'%d.%m.%Y%H:%M:%S')
-    self.time = np.linspace(0, (self.end - self.start).total_seconds(), 
+    self.time = np.linspace(0, (self.end - self.start).total_seconds(),
             int(self.header['DATASIZEROWS']))
     self.data = np.zeros([int(self.header['DATASIZEROWS']), int(self.header['DATASIZECOLS'])])
     fileObj.read(2) #first two bytes are not data
@@ -773,9 +776,9 @@ def load_nvl(filePath):
 
 def load_asc(filePath):
     '''UNTESTED - Load ASCII files into python.'''
-    try: 
+    try:
         fileObj = open(filePath, 'rb')
-    except:  
+    except:
         raise NameError('File not found.')
     self = Spy()
     header= {}
@@ -804,7 +807,7 @@ def load_asc(filePath):
     fileObj.close()
     return self
 
-    
+
 ####    ____CLASS DEFINITIONS____   ####
 
 class Spy(object):
