7f9b2678c40c83e89bcc0242e558d95e0a01c2f6
harrispirie <hpirie@live.com>
2020-05-21 08:55:48 +1200

    New stmpy 101 tutorial
    Check it out!

--- stmpy/__init__.py
+++ stmpy/__init__.py
@@ -4,6 +4,7 @@ from stmpy.io import load, save
 from stmpy import tools
 from stmpy import matio
 from stmpy import image
+from stmpy import driftcorr as drift
 from stmpy.image import saturate
 from stmpy.color.colormap import cm
 import stmpy.color.palette as palette
