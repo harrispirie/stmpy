32d9f11e4f4c77f1ad7916b3d4c8e687b1220f47
harrispirie <hpirie@live.com>
2020-06-02 10:44:05 +1200

    Added stmpy.drift module to import settings
    Users missing opencv and skimage will get a warning that these packages are required.

--- stmpy/__init__.py
+++ stmpy/__init__.py
@@ -4,7 +4,16 @@ from stmpy.io import load, save
 from stmpy import tools
 from stmpy import matio
 from stmpy import image
-# from stmpy import driftcorr as drift
+try:
+    from stmpy import driftcorr as drift
+except ModuleNotFoundError:
+    print(
+    '''
+    WARNING: Drift correciton has not been imported due to missing modules.
+    Please install opencv and scikit-image using:
+    \npip3 install opencv-python scikit-image\n
+    '''
+    )
 from stmpy.image import saturate
 from stmpy.color.colormap import cm
 import stmpy.color.palette as palette
