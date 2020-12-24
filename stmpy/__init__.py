47c1138bc7bf400cea6317fb4e7d445691f51536
Let0n <gejianfeng@g.harvard.edu>
2020-12-24 15:28:59 +0100

    SyntaxWarning issue python3.8+

--- stmpy/__init__.py
+++ stmpy/__init__.py
@@ -1,3 +1,7 @@
+import warnings
+warnings.filterwarnings("ignore", category=SyntaxWarning)
+#For Python 3.8+, to avoid SyntaxWarning: "is" with a literal. Did you mean "=="?
+
 __all__ = ['tools']
 
 from stmpy.io import load, save
